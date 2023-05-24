62a0ac7b2fabac404efe551b75f6ed9319feac62
Sanghee0820 <lsh277604@gmail.com>
2023-05-24 13:45:41 +0900

    init_server

---
+++ src/main/java/com/Giftical/Gictical_server/GicticalServerApplication.java
@@ -0,0 +1,13 @@
+package com.Giftical.Gictical_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GicticalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GicticalServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Giftical/Gictical_server/GicticalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Giftical.Gictical_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GicticalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
