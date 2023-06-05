b191d5fea53967bbf535f3a6ead670d3862af2c5
SangHee Lee <lsh277604@gmail.com>
2023-06-06 03:43:03 +0900

    change DB

--- src/main/java/com/Giftical/Giftical/User/UserController.java
+++ src/main/java/com/Giftical/Giftical/User/UserController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
-import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
-
 @RestController
 @RequiredArgsConstructor
 public class UserController {
